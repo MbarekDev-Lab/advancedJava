9778c90e525eba8d28adddaf59ab2f79067259a8
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-03-15 11:42:49 +0100

    race condition when reading counter1 and counter2 in getcounter1() and getcounter2()

--- java-se-programming/R_multithreading/src/concurrency/ResourceSharing.java
+++ java-se-programming/R_multithreading/src/concurrency/ResourceSharing.java
@@ -6,29 +6,81 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ResourceSharing {
 
     public static void main(String[] args) throws InterruptedException {
-        InventoryCounter inventoryCounter = new InventoryCounter();
-
+        /*InventoryCounter inventoryCounter = new InventoryCounter();
         IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
         DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
-
         IncrementingThread incrementingThread2 = new IncrementingThread(inventoryCounter);
         DecrementingThread decrementingThread2 = new DecrementingThread(inventoryCounter);
-
         incrementingThread.start();
         decrementingThread.start();
         incrementingThread.join();
         decrementingThread.join();
-
         incrementingThread2.start();
         decrementingThread2.start();
         incrementingThread2.join();
         decrementingThread2.join();
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");*/
 
-        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+        SharedClass sharedObject = new SharedClass();
 
+        Thread thread1 = new Thread(() -> {
+            while (true) {
+                sharedObject.incrementCounter1();
+            }
+        });
 
+        Thread thread2 = new Thread(() -> {
+            while (true) {
+                sharedObject.incrementCounter2();
+            }
+        });
 
+        thread1.start();
+        thread2.start();
+
+        // Delay to allow the threads to modify the counters
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("getcounter1: " + sharedObject.getcounter1() + " counter1");
+        System.out.println("getcounter2: " + sharedObject.getcounter2() + " counter2");
     }
+
+    static class SharedClass {
+        private int counter1 = 0;
+        private int counter2 = 0;
+
+        private final Object lock1 = new Object();
+        private final Object lock2 = new Object();
+
+        public void incrementCounter1() {
+            synchronized (lock1) {
+                counter1++;
+            }
+        }
+
+        public void incrementCounter2() {
+            synchronized (lock2) {
+                counter2++;
+            }
+        }
+
+        public int getcounter1() {
+            synchronized (lock1) { // Ensure thread safety
+                return counter1;
+            }
+        }
+
+        public int getcounter2() {
+            synchronized (lock2) { // Ensure thread safety
+                return counter2;
+            }
+        }
+    }
+
 
     public static class DecrementingThread extends Thread {
         private final InventoryCounter inventoryCounter;
@@ -88,6 +140,7 @@ public class ResourceSharing {
             return items;
         }
     }
+
 
 
 

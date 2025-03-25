beb76552b8b7e592c4f3db49ef7cfe0c4062cef5
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-03-25 07:34:02 +0100

    Insider thread: VirtualThread[#1025]/runnable@ForkJoinPool-1-worker-16

---
+++ java-se-programming/R_multithreading/src/concurrency/VertualThreadsDemo.java
@@ -0,0 +1,36 @@
+package concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VertualThreadsDemo {
+
+    private static final int NUMBER_OF_THREADS = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> System.out.println("Insider thread: " + Thread.currentThread());
+        //  runnable.run();
+        // Thread platformThread = new Thread(runnable);
+        // Thread platformThread = Thread.ofPlatform().unstarted(runnable);
+        // Insider thread: Thread[#30,Thread-0,5,main]
+        //  id - #30  , Thread-0,  Priority 5 (default of any thread ) , parent iof this thread is main thread.
+        List<Thread> vertualTreadList = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            Thread virtualThread = Thread.ofVirtual().unstarted(runnable);
+            vertualTreadList.add(virtualThread);
+        }
+        for (Thread vt : vertualTreadList) {
+            vt.start();
+        }
+        for (Thread vt : vertualTreadList) {
+            vt.join();
+        }
+        /*
+            Insider thread: VirtualThread[#30]/runnable@ForkJoinPool-1-worker-1
+            Insider thread: VirtualThread[#31]/runnable@ForkJoinPool-1-worker-2
+          the highest  Insider thread: VirtualThread[#1025]/runnable@ForkJoinPool-1-worker-16 (because i have 16 cors on my computere)
+         */
+
+
+    }
+}

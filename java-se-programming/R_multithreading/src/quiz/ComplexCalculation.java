9434a6b6e5a890e90bf6ba091da6ced255a00795
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-03-30 19:31:52 +0200

    ComplexCalculation

--- java-se-programming/R_multithreading/src/quiz/ComplexCalculation.java
+++ java-se-programming/R_multithreading/src/quiz/ComplexCalculation.java
@@ -17,7 +17,7 @@ public class ComplexCalculation {
             thread1.join();
             thread2.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
         // Combine results

28a12e5c259181a4894043cdc6eb417ebb75cefd
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-04-01 11:04:50 +0200

    False sharing in Java

---
+++ java-se-programming/R_multithreading/src/performence/SharedResource.java
@@ -0,0 +1,8 @@
+package performence;
+
+class SharedResource {
+    public volatile long var1;
+    public volatile long var2;
+    public volatile long count1 = 0;
+    public volatile long count2 = 0;
+}

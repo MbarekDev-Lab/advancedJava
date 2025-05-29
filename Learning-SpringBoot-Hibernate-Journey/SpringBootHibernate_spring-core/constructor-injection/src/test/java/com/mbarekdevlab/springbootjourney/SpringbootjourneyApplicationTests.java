0b3b7d24d4ea0e6136e78a44bc5bb93b75565a22
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-05-29 21:17:00 +0200

    Dependency Injection (Constructor Injection)

---
+++ Learning-SpringBoot-Hibernate-Journey/SpringBootHibernate_spring-core/constructor-injection/src/test/java/com/mbarekdevlab/springbootjourney/SpringbootjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbarekdevlab.springbootjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
